e7dc44ce26ec7e714d253b1a0b4479bc3d3ca509
hubertmarszalek <hubert.mar05@gmail.com>
2026-02-09 00:39:30 +0100

    car

--- rozdział 9/car.py
+++ rozdział 9/car.py
@@ -11,8 +11,18 @@ class Car():
     def read_odometer(self):
         print(f"ten samochod ma przejechane {self.odometer_reading} km")
 
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("nie mozna cofnąć przebiegu")
+
 a3 = Car("Audi", "A3", 2020)
 print(a3.full_name())
 a3.read_odometer()
 a3.odometer_reading = 200
+a3.read_odometer()
+a3.update_odometer(2000)
+a3.read_odometer()
+a3.update_odometer(1500)
 a3.read_odometer()
